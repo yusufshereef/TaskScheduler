219075eef3b849f0ec87b3122966b4683531f7fb
yusuf shereef <yusufshereef69@gmail.com>
2026-02-15 14:30:06 +0530

    added createTask method(v.1)

--- src/taskscheduler/TaskManager.java
+++ src/taskscheduler/TaskManager.java
@@ -1,7 +1,13 @@
 package taskscheduler;
 
+import taskscheduler.datastructures.hashtable.HashTable;
+
 public class TaskManager {
+    private HashTable hashTable = new HashTable();
     public void createTask(String task_name, String deadline, int priority){
-        Task task = new Task(task_name, deadline, priority);
+        if(hashTable.get(task_name)==null){
+            Task task = new Task(task_name, deadline, priority);
+            hashTable.put(task);
+        }
     }
 }
