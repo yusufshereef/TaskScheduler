8e2060c278ce3d8425e32a42cf034923c1b59a10
yusuf shereef <yusufshereef69@gmail.com>
2026-02-15 14:28:58 +0530

    added getter to taskName

--- src/taskscheduler/Task.java
+++ src/taskscheduler/Task.java
@@ -6,16 +6,20 @@ import java.time.format.DateTimeFormatter;
 public class Task {
     private static int count;
     private final int id;
-    private String task_name;
+    private String taskName;
     private LocalDateTime deadline;
     private int priority;
     private int status;
 
     public Task(String task_name, String deadline, int priority) {
-        this.task_name = task_name.trim().toLowerCase();
+        this.taskName = task_name.trim().toLowerCase();
         this.deadline = LocalDateTime.parse(deadline, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
         this.priority = priority;
         this.status = 0;
         this.id = ++count;
+    }
+
+    public String getTaskName() {
+        return taskName;
     }
 }
