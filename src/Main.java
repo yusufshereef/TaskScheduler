32595652c34684294f050c2b2bd1c5d5b3b8d970
yusuf shereef <yusufshereef69@gmail.com>
2026-02-15 11:18:15 +0530

    test commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hi");
+        System.out.println("test ");
+
     }
 }
