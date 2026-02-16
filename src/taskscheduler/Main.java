724c330e671ff005964b507f7b0b67a790f1678c
yusuf shereef <yusufshereef69@gmail.com>
2026-02-16 22:14:26 +0530

    modified menu with update function

--- src/taskscheduler/Main.java
+++ src/taskscheduler/Main.java
@@ -13,12 +13,15 @@ public class Main {
             System.out.println("=================================");
             System.out.println("1. Create Task");
             System.out.println("2. Delete Task");
-            System.out.println("3. display: ");
+            System.out.println("3. display ");
+            System.out.println("4. update task details ");
             System.out.println("8. exit: ");
             System.out.print("Enter your choice: ");
             ch = sc.nextInt();
             sc.nextLine();
             String taskName;
+            String deadline;
+            int priority;
             switch (ch){
                 case 1:
                     System.out.print("enter number of tasks you want to create: ");
@@ -28,9 +31,9 @@ public class Main {
                         System.out.print("\nEnter Task: ");
                         taskName = sc.nextLine();
                         System.out.print("\nEnter deadline (yyyy-MM-dd HH:mm): ");
-                        String deadline = sc.nextLine();
+                        deadline = sc.nextLine();
                         System.out.print("\nEnter priority (1-HIGH, 2-LOW): ");
-                        int priority = sc.nextInt();
+                        priority = sc.nextInt();
                         sc.nextLine();
                         tm.createTask(taskName, deadline, priority);
                     }
@@ -44,7 +47,27 @@ public class Main {
                     tm.displayTasks();
                     break;
                 case 4:
-
+                    System.out.println("1. update task deadline");
+                    System.out.println("2. update task priority");
+                    System.out.print("enter choice: ");
+                    int choice = sc.nextInt();
+                    sc.nextLine();
+                    switch(choice){
+                        case 1:
+                            System.out.print("\nEnter task name(to be updated): ");
+                            taskName = sc.nextLine();
+                            System.out.print("\nEnter new deadline (yyyy-MM-dd HH:mm):");
+                            deadline = sc.nextLine();
+                            tm.updateTaskDeadline(taskName, deadline);
+                            break;
+                        case 2:
+                            System.out.print("\nEnter task name(to be updated): ");
+                            taskName = sc.nextLine();
+                            System.out.print("\nEnter new priority(1-HIGH, 2-LOW) : ");
+                            priority = sc.nextInt();
+                            tm.updateTaskPriority(taskName, priority);
+                            break;
+                    }
             }
         }while(ch!=8);
     }
