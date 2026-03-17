8292a2516386170ad7de9cdfea6ea48328ba13ce
yusuf shereef <yusufshereef69@gmail.com>
2026-03-17 23:04:03 +0530

    add task time input and undo functionality to task management

--- src/taskscheduler/Main.java
+++ src/taskscheduler/Main.java
@@ -19,13 +19,16 @@ public class Main {
             System.out.println("5. update task details ");
             System.out.println("6. sort tasks by deadline");
             System.out.println("7. sort tasks by priority");
-            System.out.println("8. save tasks to file: ");
-            System.out.println("9. exit");
+            System.out.println("8. show tasks by date");
+            System.out.println("9. save tasks to file");
+            System.out.println("10. undo last action");
+            System.out.println("11. exit");
             System.out.print("Enter your choice: ");
             ch = sc.nextInt();
             sc.nextLine();
             String taskName;
             String deadline;
+            String taskTime;
             int priority;
             switch (ch){
                 case 1:
@@ -37,10 +40,12 @@ public class Main {
                         taskName = sc.nextLine();
                         System.out.print("\nEnter deadline (yyyy-MM-dd HH:mm): ");
                         deadline = sc.nextLine();
+                        System.out.print("\nEnter exact task time (yyyy-MM-dd HH:mm): ");
+                        taskTime = sc.nextLine();
                         System.out.print("\nEnter priority (1-HIGH, 2-LOW): ");
                         priority = sc.nextInt();
                         sc.nextLine();
-                        tm.createTask(taskName, deadline, priority);
+                        tm.createTask(taskName, deadline, taskTime, priority);
                     }
                     break;
                 case 2:
@@ -58,7 +63,8 @@ public class Main {
                     break;
                 case 5:
                     System.out.println("1. update task deadline");
-                    System.out.println("2. update task priority");
+                    System.out.println("2. update task time");
+                    System.out.println("3. update task priority");
                     System.out.print("enter choice: ");
                     int choice = sc.nextInt();
                     sc.nextLine();
@@ -73,11 +79,20 @@ public class Main {
                         case 2:
                             System.out.print("\nEnter task name(to be updated): ");
                             taskName = sc.nextLine();
+                            System.out.print("\nEnter new task time (yyyy-MM-dd HH:mm):");
+                            taskTime = sc.nextLine();
+                            tm.updateTaskTime(taskName, taskTime);
+                            break;
+                        case 3:
+                            System.out.print("\nEnter task name(to be updated): ");
+                            taskName = sc.nextLine();
                             System.out.print("\nEnter new priority(1-HIGH, 2-LOW) : ");
                             priority = sc.nextInt();
+                            sc.nextLine();
                             tm.updateTaskPriority(taskName, priority);
                             break;
                     }
+                    break;
                 case 6:
                     System.out.println();
                     System.out.println("Tasks sorted by deadline");
@@ -89,10 +104,18 @@ public class Main {
                     tm.sortByPriority();
                     break;
                 case 8:
+                    System.out.print("Enter date (yyyy-MM-dd): ");
+                    String currentDate = sc.nextLine();
+                    tm.displayTasksByDate(currentDate);
+                    break;
+                case 9:
                     tm.save();
                     break;
+                case 10:
+                    tm.undoLastAction();
+                    break;
             }
-        }while(ch!=9);
+        }while(ch!=11);
         tm.save();
     }
 }
